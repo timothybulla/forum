aa75654921b610563c9836055767bd2bdf8fb1c9
timothybulla <83347577+timothybulla@users.noreply.github.com>
2021-07-21 17:31:48 -0400

    Completed

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-var button = document.getElementById('submit')
-
-button.addEventListener('click', function (){
+document.getElementById('submit').addEventListener('click', function (){
   
-  var nameInput = document.getElementById("name").value;
-  var messageInput = document.getElementById("message").value;
-
-  var postsDiv = document.querySelector('.post');
-  var newPosts = document.createElement('div');
-
-  
+  var nameInput = document.getElementById('name').value;
   var newPostName = document.createElement('p');
-  var nameInputElement = document.createTextNode('Posted by: ' + nameInput);
+  var nameInputElement = document.createTextNode('Posted By: ' + nameInput);
   newPostName.appendChild(nameInputElement);
 
-  
+  var messageInput = document.getElementById('message').value;
   var newPostMessage = document.createElement('p');
   var messageInputElement = document.createTextNode(messageInput);
   newPostMessage.appendChild(messageInputElement);
 
-  newPosts.append(newPostName);
+  var postsDiv = document.querySelector('.posts');
+
+  var newPosts = document.createElement('div');
+  
+  var newPostDivider = document.createElement('hr');
+
+
   newPosts.append(newPostMessage);
+  newPosts.append(newPostName);
+  newPosts.append(newPostDivider);
+
+  postsDiv.append(newPosts);
 
 });
-
