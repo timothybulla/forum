dcea5fcc2470a39f5b8acaf76331ffce074ad35e
timothybulla <83347577+timothybulla@users.noreply.github.com>
2021-07-21 15:54:18 -0400

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-// your code here
+var button = document.getElementById=("submit")[0];
+
+button.addEventListener('click', function (){
+
+  var nameInput = document.getElementById("name")[0].value;
+  var newName = document.createElement("newName");
+  var nameInputElement = document.createTextNode(nameInput);
+
+  var messageInput = document.getElementById("message")[0].value;
+  var newMessage = document.createElement(newMessage);
+  var messageInputElement = document.createTextNode(messageInput);
+
+  newName.appendChild(nameInput);
+  newMessage.appendChild(messageInput);
+
+  document.getElementsByClassName("submissionName",)[0].append(newName);
+  document.getElementsByClassName("submissionMessage",)[0].append(newMessage);
+
+});
